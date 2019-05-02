3aa7b60302e6be0b4e4dbedfc1bb86826749e6a3
Shawn Dotey <scranet@gmail.com>
2019-05-02 14:49:42 -0400

    angular 8 super slow compile time

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { AppBrowserModule } from '.././src/app/app.browser.module';
 import { enableProdMode } from '@angular/core';
+import { AppBrowserModule } from '.././src/app/app.browser.module';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+  enableProdMode
 }
 
 platformBrowserDynamic().bootstrapModule(AppBrowserModule)
